29863b06c8d62c3fa46495177ea20472540bfb23
qu_zhen_hao <quzhenhao100@163.com>
2020-01-20 16:07:17 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
 'use strict'
 const path = require('path')
+const defaultSettings = require('./src/settings.js')
 
-function resolve (dir) {
+function resolve(dir) {
   return path.join(__dirname, dir)
 }
+
+const name = defaultSettings.title || 'vue Element Admin' // page title
+
+// If your port is set to 80,
+// use administrator privileges to execute the command line.
+// For example, Mac: sudo npm run
+// You can change the port by the following method:
+// port = 9527 npm run dev OR npm run dev --port = 9527
+const port = process.env.port || process.env.npm_config_port || 9527 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
@@ -14,43 +24,50 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '',
+  publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
-  lintOnSave: false,
-  // lintOnSave: process.env.NODE_ENV === 'development',
+  lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
-    port: 8080,
-    host: 'localhost',
+    port: port,
     open: true,
     overlay: {
       warnings: false,
       errors: true
     },
-    // sockHost: 'http://localhost:8001/',
-    disableHostCheck: true,
-    proxy: {
-      // change xxx-api/login => mock/login
-      // detail: https://cli.vuejs.org/config/#devserver-proxy
-      [process.env.VUE_APP_BASE_API]: {
-        target: 'http://zzfx.hoptest.haier.net',
-        changeOrigin: true,
-        pathRewrite: { },
-        ws: false
-      }
-    }
+    before: require('./mock/mock-server.js')
   },
   configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
     resolve: {
       alias: {
         '@': resolve('src')
       }
     }
   },
-  chainWebpack (config) {
+  chainWebpack(config) {
     config.plugins.delete('preload') // TODO: need test
     config.plugins.delete('prefetch') // TODO: need test
+
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
 
     // set preserveWhitespace
     config.module
@@ -64,7 +81,7 @@ module.exports = {
       .end()
 
     config
-    // https://webpack.js.org/configuration/devtool/#development
+      // https://webpack.js.org/configuration/devtool/#development
       .when(process.env.NODE_ENV === 'development',
         config => config.devtool('cheap-source-map')
       )
